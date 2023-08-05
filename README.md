93f42b970acaddf2a27d67874c1b18001a825b46
Muhannad Barhoum <MuhannadOBarhoum@gmail.com>
2023-08-05 19:31:33 +0300

    update readme

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
-# water
+# Water
+A Flutter project to track water drinking.
 
-A new Flutter project.
+## Features
 
-## Getting Started
+- Beautiful minimal UI
+- Display drank water
+- Set daily target to amount of water to be drunk
+- Show history of drink water
 
-This project is a starting point for a Flutter application.
+## Screenshots
 
-A few resources to get you started if this is your first Flutter project:
+<table>
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+  <tr> 
+    <td> <img width="250px" src="screenshots/home.png">  </td>
+    <td> <img width="250px" src="screenshots/history.png">  </td>
+    <td> <img width="250px" src="screenshots/settings.png">  </td>
+  </tr>
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+</table>
--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -19,7 +19,6 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
   @override
   void initState() {
     super.initState();
-
     WidgetsBinding.instance.addObserver(this);
   }
 
@@ -30,6 +29,7 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
   }
 
   DateTime? lastUpdated;
+
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     if (state == AppLifecycleState.resumed) {
@@ -44,38 +44,13 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
     }
   }
 
-// HomePage() {
-  //   _initQuickActions();
-  // }
-
-  // void _initQuickActions() {
-  //   final QuickActions quickActions = const QuickActions();
-  //   quickActions.initialize((String shortcutType) {
-  //     // StoreConnector<AppState, AppState>(converter: (Store store) {}, builder: (BuildContext context, AppState vm) {},);
-  //     if (shortcutType == 'add_small_water') {
-  //       Drink drink = Drink.water();
-  //       var entry = DrinkHistoryEntry();
-  //       entry.amount = drink.amount;
-  //       entry.date = DateTime.now().millisecondsSinceEpoch;
-  //       // store.dispatch(AddDrinkToHistoryAction(entry));
-  //     }
-  //   });
-
-  //   quickActions.setShortcutItems(<ShortcutItem>[
-  //     const ShortcutItem(
-  //       type: 'add_small_water',
-  //       localizedTitle: 'Small Water (250 ml)',
-  //     ),
-  //   ]);
-  // }
-
   Widget _getBody(int index) {
     switch (index) {
       case 0:
         return const TodayPage();
       case 1:
         return const HistoryPage();
-      case 3:
+      case 2:
         return const SettingsPage();
       default:
         return const TodayPage();
@@ -97,23 +72,15 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
               ),
               label: 'Today'),
           BottomNavigationBarItem(
-              icon: ShadowIcon(
-                Icons.history,
-                offsetX: 0.0,
-                offsetY: 0.0,
-                blur: 3.0,
-                shadowColor: Colors.black.withOpacity(0.25),
-              ),
-              label: 'History'),
-          BottomNavigationBarItem(
-              icon: ShadowIcon(
-                Icons.notifications,
-                offsetX: 0.0,
-                offsetY: 0.0,
-                blur: 3.0,
-                shadowColor: Colors.black.withOpacity(0.25),
-              ),
-              label: 'Notifications'),
+            icon: ShadowIcon(
+              Icons.history,
+              offsetX: 0.0,
+              offsetY: 0.0,
+              blur: 3.0,
+              shadowColor: Colors.black.withOpacity(0.25),
+            ),
+            label: 'History',
+          ),
           BottomNavigationBarItem(
             icon: ShadowIcon(
               Icons.settings,
@@ -134,23 +101,26 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
         return CupertinoTabView(
           builder: (BuildContext context) {
             return CupertinoPageScaffold(
-                backgroundColor: const Color(0xFFf7f7f7),
-                child: Stack(
-                  children: <Widget>[
-                    Container(
-                      decoration: const BoxDecoration(
-                          image: DecorationImage(
-                              alignment: AlignmentDirectional.topStart,
-                              image: AssetImage(
-                                  'assets/background/top-background.png'),
-                              fit: BoxFit.fitWidth)),
+              backgroundColor: const Color(0xFFf7f7f7),
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        alignment: AlignmentDirectional.topStart,
+                        image:
+                            AssetImage('assets/background/top-background.png'),
+                        fit: BoxFit.fitWidth,
+                      ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 40),
-                      child: _getBody(index),
-                    ),
-                  ],
-                ));
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 40),
+                    child: _getBody(index),
+                  ),
+                ],
+              ),
+            );
           },
         );
       },
